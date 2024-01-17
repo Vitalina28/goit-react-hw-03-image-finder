9a40d588e5429293f06a773144c87b392d6bb503
Vitalina28 <132218868+Vitalina28@users.noreply.github.com>
2024-01-17 22:29:23 +0100

    add jsx and css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,80 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+import axios from 'axios';
+import toast, { Toaster } from 'react-hot-toast';
+
+import { Searchbar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Modal } from './Modal/Modal';
+import { Loader } from './Loader/Loader';
+import { Button } from './Button/Button';
+
+const PER_PAGE = 12;
+
+export default class App extends Component {
+  state = {
+    searchQuery: '',
+    images: [],
+    page: 1,
+    isLoading: false,
+    selectedImage: '',
+  };
+
+  handleFormSubmit = searchQuery => {
+    this.setState({ searchQuery });
+  };
+
+  async componentDidUpdate(_, prevState) {
+    const BASE_URL = 'https://pixabay.com/api/';
+    const API_KEY = '40708369-d6dc8a1f6c8c52c3bc15ef748';
+
+    if (
+      prevState.searchQuery !== this.state.searchQuery ||
+      prevState.page !== this.state.page
+    ) {
+      try {
+        this.setState({ isLoading: true });
+        const response = await axios.get(
+          `${BASE_URL}?key=${API_KEY}&q=${this.state.searchQuery}&orientation=horizontal&per_page=${PER_PAGE}&page=${this.state.page}`
+        );
+        this.setState({ images: response.data.hits });
+      } catch (error) {
+        toast.error('Reload the page and try again');
+      } finally {
+        this.setState({ isLoading: false });
+      }
+    }
+  }
+
+  handleChangePage = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
+  handleOpenModal = largeImageURL => {
+    this.setState({ selectedImage: largeImageURL });
+  };
+
+  handleCloseModal = () => {
+    this.setState({ selectedImage: '' });
+  };
+
+  render() {
+    const { images, isLoading, selectedImage } = this.state;
+
+    return (
+      <>
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        <ImageGallery images={images} onClick={this.handleOpenModal} />
+        {isLoading && <Loader />}
+        {images.length > 0 && <Button onClick={this.handleChangePage} />}
+        <Toaster />
+        {selectedImage && (
+          <Modal
+            onClose={this.handleCloseModal}
+            selectedImage={selectedImage}
+          />
+        )}
+      </>
+    );
+  }
+}
---
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -0,0 +1,14 @@
+import css from './ImageGalleryItem.module.css';
+
+export const ImageGalleryItem = ({ image, onClick }) => {
+  return (
+    <li className={css.galleryItem} onClick={() => onClick(image)}>
+      <img
+        className={css.galleryItemImage}
+        src={image.webformatURL}
+        alt={image.tags}
+        width="250"
+      />
+    </li>
+  );
+};
